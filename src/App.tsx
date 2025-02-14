aa6b27f66def97fc83036c5ff799623554489ff6
pwccaa <mattos1@mx2.unisc.br>
2025-02-14 00:15:12 -0300

    refactor: move SpellList to HomePage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import SpellList from "./components/SpellList";
+import "./App.css";
+import spells from "./data/spells.json";
+import HomePage from "pages/HomePage";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/spellbook" element={<HomePage />} />
       </Routes>
     </Router>
   );
